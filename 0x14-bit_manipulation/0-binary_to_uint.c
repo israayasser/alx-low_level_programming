426fb1d4c3626a5c642caecb01e8b9f3f269b918
israayasser <esraayasser809@gmail.com>
2024-02-08 18:49:02 +0200

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,15 @@ unsigned int binary_to_unit(const char *b)
 	unsigned int num = 0;
 
 	if (!b)
-	return (0);
+	{
+		return (0);
+	}
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
-		return (0);
+		{
+			return (0);
+		}
 		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
